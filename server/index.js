579286383e13d794f77338751479880dfe2e55f0
VIJAY KUMAR <vijay7dnc@gmail.com>
2021-04-02 19:37:02 +0530

    add the web app

---
+++ server/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+const config = require("./config/key");
+
+
+
+const mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost/web_app');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.use('/api/users', require('./routes/users'));
+app.use('/api/video', require('./routes/video'));
+app.use('/api/subscribe', require('./routes/subscribe'));
+app.use('/api/comment', require('./routes/comment'));
+
+app.use('/uploads', express.static('uploads'));
+
+
+if (process.env.NODE_ENV === "production") {
+
+  
+  app.use(express.static("client/build"));
+
+  
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
+  console.log(`Server Running at ${port}`)
+});
